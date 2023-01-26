76182f962c40fa5385926a17e2aa60f63290ad3c
Jaroslav Málek <malekjaroslav@email.cz>
2023-01-26 17:51:14 +0100

    add comment

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function run() {
+  // When "take" and "orderBy" is included, typescript shows an error
   const contract = await prisma.rentContract.findUniqueOrThrow({
     where: {
       id: "",
@@ -22,4 +23,25 @@ async function run() {
   });
 
   contract.versions[0]?.tenant.name;
+
+  // When "take" and "orderBy" is excluded, typescript shows correct suggestions
+  const contract2 = await prisma.rentContract.findUniqueOrThrow({
+    where: {
+      id: "",
+    },
+    include: {
+      tenant: true,
+      versions: {
+        /* take: 1,
+        orderBy: {
+          createdAt: "desc",
+        },*/
+        include: {
+          tenant: true,
+        },
+      },
+    },
+  });
+
+  contract2.versions[0]?.tenant.name;
 }
